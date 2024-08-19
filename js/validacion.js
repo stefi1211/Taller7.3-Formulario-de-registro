e90b118e89f172cfbded5bd6b1017814fc37499b
Stefi <144570099+stefi1211@users.noreply.github.com>
2024-08-19 13:43:42 +0000

    validacion de datos

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,35 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function() {
+    
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password1 = document.getElementById("password1").value.trim();
+    const password2 = document.getElementById("password2").value.trim();
+    const terminos = document.getElementById("terminos").checked;
+
+    if (!nombre || !apellido || !email || !password1 || !password2) {
+        showAlertError();
+        return;
+    }
+
+    if (password1.length < 6) {
+        showAlertError();
+        return;
+    }
+
+    if (password1 !== password2) {
+        showAlertError();
+        return;
+    }
+
+    if (!terminos) {
+        showAlertError();
+        return;
+    }
+
+    showAlertSuccess();
+});
